19dcc893dbc543717f01ddc9892256972ec634a4
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-03-17 18:20:03 +0800

    people can't see edit button unless he is group owner

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,3 +26,19 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
+  def update
+      @group = Group.find(params[:id])
+      if @group.update(group_params)
+           redirect_to groups_path, notice: "Update Success"
+         else
+           render :edit
+         end
+      end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
