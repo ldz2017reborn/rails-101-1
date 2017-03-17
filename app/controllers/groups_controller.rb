ebc0691d88cc352c3b36dc09db1aa544fca6d5ca
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-03-17 08:55:03 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    
+
     if @group.save
         redirect_to groups_path
     else
@@ -30,10 +30,11 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
