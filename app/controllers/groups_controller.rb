92b6aa47ddae47a56dc5ba8511c9388e4c5e0882
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-03-17 11:33:59 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,11 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!,only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
   end
 
-  def show
-     @group = Group.find(params[:id])
+  def new
+    @group = Group.new
   end
 
   def show
@@ -16,9 +16,6 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def new
-    @group = Group.new
-  end
   def create
     @group = Group.new(group_params)
     @group.user = current_user
